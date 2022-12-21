1a6a05d98d267d58efdaa9ed88cd0c3bb0ad6a80
Rache Melendres <rachemelendres@gmail.com>
2022-12-21 13:14:44 +0800

    changed to square buttons

--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 <nav class="nav w3-col l2 w3-card w3-sidebar w3-bar-block" style="z-index: 1;"> 
 
 
-<a href="./?page=home"><div id = 'home-button' class="w3-bar-item w3-button w3-round"> &nbsp; Home</div></a>
-<a href="./?page=regional"><div id = 'regional-button' class="w3-bar-item w3-button w3-round"> &nbsp; Regional KPI Trends</div></a>
-<a href="./?page=market-kpi"><div id = 'market-kpi-button' class="w3-bar-item w3-button w3-round"> &nbsp; Market - Project KPIs</div></a>
-<a href="./?page=market"><div id = 'market-button' class="w3-bar-item w3-button w3-round"> &nbsp; Market - GIA Projects</div></a>
+<a href="./?page=home"><div id = 'home-button' class="w3-bar-item w3-button"> &nbsp; Home</div></a>
+<a href="./?page=regional"><div id = 'regional-button' class="w3-bar-item w3-button"> &nbsp; Regional KPI Trends</div></a>
+<a href="./?page=market-kpi"><div id = 'market-kpi-button' class="w3-bar-item w3-button"> &nbsp; Market - Project KPIs</div></a>
+<a href="./?page=market"><div id = 'market-button' class="w3-bar-item w3-button"> &nbsp; Market - GIA Projects</div></a>
 
 </div>
 
